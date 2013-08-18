77aa929fae7707c007e5c9d253ca5dc31b3f421b
taka <taka@idancoffee.com>
2013-08-18 16:05:07 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tr7monitor::Application.config.secret_key_base = 'cb0ba1a2896ed2405509eacaf169d090be81d7fbe30399ea6ca1913eb4bf8a2ca856bcf861a9641fe214a73ad00df3eb251e7c98bd4d002f4ed019af9de6bd81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tr7monitor::Application.config.session_store :cookie_store, key: '_tr7monitor_session'
